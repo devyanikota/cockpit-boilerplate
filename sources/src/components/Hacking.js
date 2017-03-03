69e5eb07bcc9a22c91b26edf1937a2bb539c0aa4
Ryan Barry <rbarry@redhat.com>
2017-03-03 07:31:18 -0700

    add a file reader

--- sources/src/components/Hacking.js
+++ sources/src/components/Hacking.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 
+import { LoremBox } from './LoremBox'
 import { Timer } from './Timer'
 
 export default class Hacking extends Component {
@@ -19,6 +20,7 @@ export default class Hacking extends Component {
           Hacking
         </h2>
         <Timer />
+        <LoremBox name="ipsum"/>
      </div>
     )
   }
